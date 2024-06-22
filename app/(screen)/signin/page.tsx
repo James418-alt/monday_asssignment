831fac963ed39f58f0c1f40e1b88ce54b7e3451a
James418-alt <ebifegha123@gmail.com>
2024-06-22 14:05:03 +0100

    fixing signin

--- app/(screen)/signin/page.tsx
+++ app/(screen)/signin/page.tsx
@@ -1,10 +1,11 @@
-"use client";
+// "use client";
 import Link from "next/link";
 import React from "react";
 import { MdLock } from "react-icons/md";
 
 const page = () => {
-  const formAction = (formData: FormData) => {
+  const formAction = async (formData: FormData) => {
+    "use server";
     const email = formData.get("email");
     const password = formData.get("password");
     console.log(email, password);
