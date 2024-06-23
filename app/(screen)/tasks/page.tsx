4605877a516289f3081eb9d8952e751eb08f0989
James418-alt <ebifegha123@gmail.com>
2024-06-23 14:52:31 +0100

    view page done

---
+++ app/(screen)/tasks/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+
+const page = async () => {
+  const url = "http://localhost:3000";
+  const res = await fetch(`${url}/api/card`, {
+    method: "GET",
+    cache: "no-cache",
+    next: {
+      tags: ["card"],
+    },
+  });
+
+  const data = await res.json();
+
+  return (
+    <div>
+      {data?.data?.map((el: any) => {
+        return (
+          <div key={el?._id}>
+            <Image alt={el?.title} src={el?.image} width={100} height={100} />
+
+            <p className="mt-1 ml-3 capitalize font-sans">{el?.title}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default page;
