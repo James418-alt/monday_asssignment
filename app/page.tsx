72e0d9a71eeb078c0113f895d9990dccd2df3ddb
James418-alt <ebifegha123@gmail.com>
2024-06-23 22:16:56 +0100

    well done james

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ import moment from "moment";
 const page = async () => {
   const res = await fetch("https://monday-asssignment.vercel.app/api/card", {
     method: "GET",
-    headers: {
-      "content-type": "application/json",
+    cache: "no-cache",
+    next: {
+      tags: ["product"],
     },
+    // headers: {
+    //   "content-type": "application/json",
+    // },
   });
   let data = await res.json();
 
